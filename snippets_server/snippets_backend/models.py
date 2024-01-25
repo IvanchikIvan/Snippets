c772bd7adaa1bb13bb62c9429254c9dc6b29d187
Kavalskiy <vanyamalisheff@yandex.ru>
2024-01-25 07:21:37 +0300

    Added Snippet model

--- snippets_server/snippets_backend/models.py
+++ snippets_server/snippets_backend/models.py
@@ -1,2 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
+
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Snippet(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.TextField(max_length=5000)
+    creation_date = models.DateTimeField()
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE,
+                             blank=True, null=True)
