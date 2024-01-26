50cbcc3732bca6fb67a8beffa6135b351614ab6f
Kavalskiy <vanyamalisheff@yandex.ru>
2024-01-26 07:23:17 +0300

    Added UserSerializer

--- snippets_server/snippets_backend/serializers.py
+++ snippets_server/snippets_backend/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Snippet
+from django.contrib.auth.models import User
 
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ['id', 'name', 'code', 'creation_date', 'user']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'password', 'last_login', 'is_superuser', 'username', 'last_name', 'email', 'is_staff', 'is_active', "date_joined", 'first_name']
