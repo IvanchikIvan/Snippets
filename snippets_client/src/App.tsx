b0c81fb06fb6cc3133774f8272fbcb7a801adc0c
Kavalskiy <vanyamalisheff@yandex.ru>
2024-01-28 23:42:45 +0300

    New Routes

--- snippets_client/src/App.tsx
+++ snippets_client/src/App.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SnippetList from './components/SnippetsList/SnippetsList';
-import Register from './components/Register/Register';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import SnippetList from "./components/SnippetsList/SnippetsList";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+import axios from "axios";
 
 const App: React.FC = () => {
+
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>();
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      setIsAuthenticated(await axios.get('http://localhost:8000/check-auth/'))
+      console.log(isAuthenticated)
+    };
+
+    checkAuthentication();
+}, []);
+
   return (
     <Router>
       <Routes>
-        <Route path="/snippets" element={<SnippetList />} />
-        <Route path="/register" element={<Register />} />
+        <Route path='/' element={ <Navigate to="/snippets" /> }/>  
+        <Route path="/snippets" element={<SnippetList/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
