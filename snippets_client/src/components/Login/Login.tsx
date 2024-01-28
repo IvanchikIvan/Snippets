cde6558f8db8f6e5bf993a7bc873792a49a3164c
Kavalskiy <vanyamalisheff@yandex.ru>
2024-01-28 23:42:18 +0300

    Login page bug fix

--- snippets_client/src/components/Login/Login.tsx
+++ snippets_client/src/components/Login/Login.tsx
@@ -1,34 +1,54 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
-const Login: React.FC = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+interface LoginProps {}
 
-    const handleRegister = async () => {
-        try {
-            const response = await axios.post('http://localhost:8000/login/', {
-                username,
-                password,
-            });
-            console.log('User logined:', response.data);
-        } catch (error) {
-            console.error('Login error:', error);
-        }
-    };
+const Login: React.FC<LoginProps> = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-    return (
-        <div>
-            <h2>Login</h2>
-            <label>Username: </label>
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-            <br />
-            <label>Password: </label>
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <br />
-            <button onClick={handleRegister}>Login</button>
-        </div>
-    );
+  const loginUser = async () => {
+    try {
+      const response = await axios.post("http://localhost:8000/login/", {
+        username,
+        password,
+      });
+      localStorage.setItem("access", response.data.access);
+      localStorage.setItem("refresh", response.data.refresh);
+      console.log("Sucsefull login")
+    } catch (err) {
+      console.error('Login error', err)
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="button" onClick={loginUser}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- snippets_server/snippets_backend/views.py
+++ snippets_server/snippets_backend/views.py
@@ -1,10 +1,11 @@
 from django.http import JsonResponse
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework import generics, status, permissions
 from rest_framework.decorators import api_view, permission_classes
 from .models import Snippet
 from django.contrib.auth.models import User
 from .serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics, permissions
+from rest_framework.response import Response
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -18,10 +19,11 @@ class RegisterView(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny]
 
 
-class LoginView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [permissions.AllowAny]
+class LoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        return Response(status=status.HTTP_200_OK)
 
 
 @api_view(['GET'])
