49e7596c51a05094a280c7158c5d16f7f25c2559
Kavalskiy <vanyamalisheff@yandex.ru>
2024-01-30 23:50:47 +0300

    Deleted unnecessary code

--- snippets_client/src/App.tsx
+++ snippets_client/src/App.tsx
@@ -1,25 +1,11 @@
-import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import SnippetList from "./components/SnippetsList/SnippetsList";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
 import { Provider } from 'react-redux';
 import store from './components/Redux/store';
-import axios from "axios";
 
 const App: React.FC = () => {
-
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>();
-
-  useEffect(() => {
-    const checkAuthentication = async () => {
-      setIsAuthenticated(await axios.get('http://localhost:8000/check-auth/'))
-      console.log(isAuthenticated)
-    };
-
-    checkAuthentication();
-}, []);
-
   return (
     <Provider store={store}>
       <Router>
