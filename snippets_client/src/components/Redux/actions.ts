7323c05a9200f4ca8966c10374acbaf42f295379
Kavalskiy <vanyamalisheff@yandex.ru>
2024-01-30 07:35:17 +0300

    Added reducer for csrf token

--- snippets_client/src/components/Redux/actions.ts
+++ snippets_client/src/components/Redux/actions.ts
@@ -1,6 +1,12 @@
 export const SET_AUTH_STATUS = 'SET_AUTH_STATUS';
+export const SET_TOKEN = 'SET_TOKEN';
 
 export const setAuthStatus = (status: boolean) => ({
   type: SET_AUTH_STATUS,
   payload: status,
+});
+
+export const setCsrfToken = (token: string) => ({
+  type: SET_TOKEN,
+  payload: token,
 });
--- snippets_client/src/components/Redux/reducers.ts
+++ snippets_client/src/components/Redux/reducers.ts
@@ -1,19 +1,28 @@
-import { SET_AUTH_STATUS } from './actions';
+import { SET_AUTH_STATUS, SET_TOKEN } from './actions';
 
 const initialState = {
   authStatus: false,
+  csrfToken: null,
 };
 
-const authReducer = (state = initialState, action: any) => {
+
+
+const rootReducer = (state = initialState, action: any) => {
   switch (action.type) {
     case SET_AUTH_STATUS:
       return {
         ...state,
         authStatus: action.payload,
       };
-    default:
-      return state;
+    case SET_TOKEN:
+        return {
+          ...state,
+          csrfToken: action.payload,
+        };
+      default:
+        return state;
   }
 };
 
-export default authReducer;
+
+export default rootReducer;
