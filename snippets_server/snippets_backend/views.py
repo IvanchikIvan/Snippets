3939c3c888b2904a84388a3ffbd5338b170678fc
Kavalskiy <vanyamalisheff@yandex.ru>
2024-01-30 23:30:33 +0300

    removed unnecessary decorator

--- snippets_server/snippets_backend/views.py
+++ snippets_server/snippets_backend/views.py
@@ -27,7 +27,6 @@ class RegisterView(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny]
 
 
-@csrf_protect
 class LoginView(APIView):
     serializer_class = UserSerializer
     def post(self, request, *args, **kwargs):
