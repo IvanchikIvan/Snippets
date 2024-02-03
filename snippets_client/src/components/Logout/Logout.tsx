d993f46fbbbde8d3607ce0b3f092d9ded6df6594
Kavalskiy <vanyamalisheff@yandex.ru>
2024-02-03 23:58:29 +0300

    Deleted unnecessary

--- snippets_client/src/components/Logout/Logout.tsx
+++ snippets_client/src/components/Logout/Logout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setAuthStatus } from '../Redux/actions';
 
 const Logout: React.FC = () => {
