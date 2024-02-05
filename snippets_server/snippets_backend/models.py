37736ffa5275b6e7839eb49c929490f1b96443a7
Kavalskiy <vanyamalisheff@yandex.ru>
2024-02-05 07:35:26 +0300

    Changed date format in Snippet model

--- snippets_server/snippets_backend/models.py
+++ snippets_server/snippets_backend/models.py
@@ -1,3 +1,4 @@
+from datetime import timezone
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -5,6 +6,6 @@ from django.db import models
 class Snippet(models.Model):
     name = models.CharField(max_length=200)
     code = models.TextField(max_length=5000)
-    creation_date = models.DateTimeField()
+    creation_date = models.DateTimeField("%Y-%m-%d %H:%M:%S")
     user = models.ForeignKey(to=User, on_delete=models.CASCADE,
                              blank=True, null=True)
