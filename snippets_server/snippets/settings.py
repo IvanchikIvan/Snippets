f9c3f2a1d9d87f66594b4883d5ba32fe3b0064f0
Kavalskiy <vanyamalisheff@yandex.ru>
2024-02-08 20:21:23 +0300

    Changed RESTFRAMWORK settings

--- snippets_server/snippets/settings.py
+++ snippets_server/snippets/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
