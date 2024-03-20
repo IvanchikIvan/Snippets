a677972a0479a8c81a41d00cf896569482864a35
Kavalskiy <vanyamalisheff@yandex.ru>
2024-03-20 17:00:21 +0300

    Updated webpack config js

---
+++ snippets_client/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/main.tsx",
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html', 
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+    ],
+  },
+  devServer: {
+    compress: true,
+    static: './dist',
+    port: 3000,
+  },
+};
