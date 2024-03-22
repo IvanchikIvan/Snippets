2edbaeb42ff9700680c324b765b41fb4391890b3
Kavalskiy <vanyamalisheff@yandex.ru>
2024-03-22 22:17:53 +0300

    Added props nad css modules for Login

--- snippets_client/src/components/Login/Login.tsx
+++ snippets_client/src/components/Login/Login.tsx
@@ -1,14 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setAuthStatus, setCsrfToken, setUsername, setUserID } from '../Redux/actions';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setAuthStatus,
+  setCsrfToken,
+  setUsername,
+  setUserID,
+} from "../Redux/actions";
+import axios from "axios";
+import styles from "./Login.module.css";
 
-axios.defaults.xsrfHeaderName = 'X-CSRFToken';
-axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
+axios.defaults.xsrfCookieName = "csrftoken";
 
-const Login: React.FC = () => {
-  const [username, setUsernameField] = useState('');
-  const [password, setPassword] = useState('');
+const Login: React.FC<{ isOpen: boolean; }> = ({isOpen}) => {
+  const [username, setUsernameField] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
@@ -16,69 +22,78 @@ const Login: React.FC = () => {
   const csrfToken = useSelector((state: any) => state.csrfToken);
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/csrf_token/')
-      .then(response => dispatch(setCsrfToken(response.data.csrf_token)))
-      .catch(error => console.error(error));
+    axios
+      .get("http://localhost:8000/api/csrf_token/")
+      .then((response) => dispatch(setCsrfToken(response.data.csrf_token)))
+      .catch((error) => console.error(error));
   }, []);
 
   const handleLogin = async () => {
     try {
       setLoading(true);
-      const response = await axios.post('http://localhost:8000/api/login/', {
-        username,
-        password,
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRFToken': csrfToken,
+      const response = await axios.post(
+        "http://localhost:8000/api/login/",
+        {
+          username,
+          password,
         },
-      });
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "X-CSRFToken": csrfToken,
+          },
+        }
+      );
 
       if (response.status === 200) {
-        
         dispatch(setAuthStatus(true));
         dispatch(setUsername(username));
-        dispatch(setUserID(response.data['user_id']))
-        localStorage.setItem('username', username);
-        localStorage.setItem('userID', response.data['user_id']);
-        localStorage.setItem('authStatus', response.data.token);
-
+        dispatch(setUserID(response.data["user_id"]));
+        localStorage.setItem("username", username);
+        localStorage.setItem("userID", response.data["user_id"]);
+        localStorage.setItem("authStatus", response.data.token);
       } else {
-        setError(response.data.error || 'Authentication failed');
+        setError(response.data.error || "Authentication failed");
       }
     } catch (error) {
-      setError('Error during login');
+      setError("Error during login");
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsernameField(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <br />
-      <button onClick={handleLogin} disabled={loading}>
-        {loading ? 'Logging in...' : 'Login'}
-      </button>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <p>{authStatus}</p>
-    </div>
+    <>
+      {!isOpen ? (
+        <></>
+      ) : (
+        <div className={styles.container}>
+          <label>
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsernameField(e.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <br />
+          <button onClick={handleLogin} disabled={loading}>
+            {loading ? "Logging in..." : "Login"}
+          </button>
+          {error && <p style={{ color: "red" }}>{error}</p>}
+          <p>{authStatus}</p>
+        </div>
+      )}
+    </>
   );
 };
 
