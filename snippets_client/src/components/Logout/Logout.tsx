57d46e6482dfb3532df775911ec8b3323d1adea4
Kavalskiy <vanyamalisheff@yandex.ru>
2024-03-22 22:19:12 +0300

    Edited Logout

--- snippets_client/src/components/Logout/Logout.tsx
+++ snippets_client/src/components/Logout/Logout.tsx
@@ -1,34 +1,40 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { setAuthStatus } from '../Redux/actions';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { setAuthStatus } from "../Redux/actions";
 
-const Logout: React.FC = () => {
+const Logout: React.FC<{ isOpen: boolean; }> = ({isOpen}) => {
   const dispatch = useDispatch();
 
   const handleLogout = async () => {
     try {
-      const response = await fetch('http://localhost:8000/api/logout/', {
-        method: 'POST',
+      const response = await fetch("http://localhost:8000/api/logout/", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       });
 
       if (response.ok) {
         dispatch(setAuthStatus(false));
-        localStorage.removeItem('authStatus');
+        localStorage.removeItem("authStatus");
       } else {
-        console.error('Logout failed');
+        console.error("Logout failed");
       }
     } catch (error) {
-      console.error('Error during logout:', error);
+      console.error("Error during logout:", error);
     }
   };
 
   return (
-    <div className="nav-link">
-      <button onClick={handleLogout}>Logout</button>
-    </div>
+    <>
+      {!isOpen ? (
+        <></>
+      ) : (
+        <div className="nav-link">
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      )}
+    </>
   );
 };
 
