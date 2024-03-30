ec04321f2e6438f78cbf4f8530235daf724e3834
Kavalskiy <vanyamalisheff@yandex.ru>
2024-03-30 23:27:17 +0300

    Fix: edit setting for .css files in webpack

--- snippets_client/webpack.config.js
+++ snippets_client/webpack.config.js
@@ -28,15 +28,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-            },
-          },
-        ],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
