30cd3ffe258e3959d8bb075fea62e4ba08a21722
Kavalskiy <vanyamalisheff@yandex.ru>
2024-04-09 22:34:09 +0300

    Feat: login is pure page now!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ import SnippetPage from "./components/SnippetPage/SnippetPage";
 import CreateSnippetForm from "./components/createSnippetPage/CreateSnippetPage";
 import MySnippetsPage from "./components/MySnippetsPage/MySnippetsPage";
 import React from "react";
+import Login from "./components/Login/Login";
 
 
 const App: React.FC = () => {
@@ -19,6 +20,7 @@ const App: React.FC = () => {
           <Route path="/register" element={<Register/>} />
           <Route path="/snippets/:id" element={<SnippetPage/>} />
           <Route path="/add_snippet" element={<CreateSnippetForm />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </Provider>
