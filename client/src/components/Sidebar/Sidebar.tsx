6eb0da53c8c7ebc22cd7bbec2b851ac1daa38270
Kavalskiy <vanyamalisheff@yandex.ru>
2024-04-09 15:26:12 +0300

    Feat: UPD sidebar

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -4,20 +4,16 @@ import { FaHome, FaUsers, FaCog, FaSignInAlt, FaPlus, FaList } from 'react-icons
 import './Sidebar.css';
 
 const Sidebar: React.FC = () => {
-  const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
-  const toggleSidebar = () => {
-    setIsExpanded(!isExpanded);
-  };
 
   return (
-    <div className={`sidebar ${isExpanded ? 'expanded' : ''}`} onMouseEnter={toggleSidebar} onMouseLeave={toggleSidebar}>
-      <SidebarItem icon={<FaHome />} title="Home" isExpanded={isExpanded} href='/'/>
-      <SidebarItem icon={<FaUsers />} title="Users" isExpanded={isExpanded} href='/'/>
-      <SidebarItem icon={<FaCog />} title="Settings" isExpanded={isExpanded} href='/'/>
-      <SidebarItem icon={<FaSignInAlt />} title="Login" isExpanded={isExpanded} href='/'/>
-      <SidebarItem icon={<FaPlus />} title="Add" isExpanded={isExpanded} href='/add_snippet/'/>
-      <SidebarItem icon={<FaList />} title="List" isExpanded={isExpanded} href='/'/>
+    <div className='sidebar' >
+      <SidebarItem icon={<FaHome />} title="Home" href='/'/>
+      <SidebarItem icon={<FaUsers />} title="Users" href='/'/>
+      <SidebarItem icon={<FaCog />} title="Settings" href='/'/>
+      <SidebarItem icon={<FaSignInAlt />} title="Login" href='/'/>
+      <SidebarItem icon={<FaPlus />} title="Add" href='/add_snippet/'/>
+      <SidebarItem icon={<FaList />} title="List" href='/'/>
     </div>
   );
 };
--- client/src/components/Sidebar/SidebarItem.tsx
+++ client/src/components/Sidebar/SidebarItem.tsx
@@ -4,20 +4,18 @@ import { Link } from 'react-router-dom';
 interface SidebarItemProps {
   icon: React.ReactNode;
   title: string;
-  isExpanded: boolean;
   href: string;
 }
 
 const SidebarItem: React.FC<SidebarItemProps> = ({
   icon,
   title,
-  isExpanded,
   href,
 }) => {
   return (
     <Link to={href} className="sidebar-item">
       <div className="icon-container">{icon}</div>
-      <span className={`title ${isExpanded ? 'expanded' : ''}`}>{title}</span>
+      <span className='title'>{title}</span>
     </Link>
   );
 };
